1621f53351e7f0e38012a2168067c98c6978f11e
foundalex <founderalexandr@gmail.com>
2025-07-09 10:57:06 +0300

    model adc

--- rls.m
+++ rls.m
@@ -1,35 +1,35 @@
 clear all;
+clc;
 
-FrameSize = 10;
-NIter = 100;
+% function [n,x,s,fs,s1] = genData(numPts, freq, filt, nVar, SNR)
+%     % Generate time values
+%     t = linspace(0,1,numPts)';
+%     fs = numPts;
+% 
+%     % Generate tone
+%     s = sin(2*pi*freq*t);
+%     s1 = sin(2*pi*freq*(t+3.4e4));
+%     % Generate noise
+%     n = sqrt(nVar)*randn(numPts,1);
+% 
+%     % Filter noise
+%     addnoise = filter(filt, 1, n);
+% 
+%     % Plot filter
+%     freqz(filt,1,1000)
+% 
+%     % Adjust SNR of tone
+%     s = s/sqrt(var(s)/(10^(SNR/10)*var(n)));
+%     disp(['Calculated SNR = ' num2str(10*log10(var(s)/var(n)))])
+% 
+%     % Add noise to signal
+%     x = s + addnoise;
+% end
 
-rls1 = dsp.RLSFilter('Length',10);
-    % 'Method','Householder RLS');
-filt = dsp.FIRFilter('Numerator',...
-    fir1(10,[.5,.75]));
-
-sinewave = dsp.SineWave('Frequency',0.01,...
-    'SampleRate',1,...
-    'SamplesPerFrame',FrameSize);
-% scope = timescope('LayoutDimensions',[2 1],...
-%     'NumInputPorts',2, ...
-%     'TimeUnits','Seconds',...
-%     'YLimits',[-2.5 2.5], ...
-%     'BufferLength',2*FrameSize*NIter,...
-%     'ActiveDisplay',1,...
-%     'ShowLegend',true,...
-%     'ChannelNames',{'Noisy signal'},...
-%     'ActiveDisplay',2,...
-%     'ShowLegend',true,...
-%     'ChannelNames',{'Error signal'});
-
-xx = zeros(1000,1);
-dd = zeros(1000,1);
-yy = zeros(1000,1);
-
-%%
-% parameter estimator menggunakan least square
-
+% % Generate the data!
+% [n,x,s,fs,s1] = genData(numPts, freq, filt, nVar, SNR);
+% % parameter estimator menggunakan least square
+% 
 % Y = ([1 : 100] + rand(1, 1))'; % proses markov orde 1
 % X = ([100 : 199] + rand(1, 1))';
 % st_time = tic();
@@ -49,190 +49,157 @@ yy = zeros(1000,1);
 % err = Y2 - Y3;
 % emse = mse(err);
 % t = 1 : length(Y3);
-% figure1;
+% figure;
 % plot(t, Y2, t, Y3);
 % title('Data Asli vs Estimasi');
-% figure2;
+% figure;
 % plot(t, err);
 % title('Galat');
-% cnsmd_time = toc(st_time);
+% cnsmd_time = toc(st_time)
 
 
+freq = 1000;             % frequency of fundamental tone
+N = 73;
+%% Time specifications:
+Fs = 20000;                 % samples per second
+dt = 1/Fs;                   % seconds per sample
+StopTime = 0.2;             % seconds
+t = (0:dt:StopTime-dt)';     % seconds
+%% Sine wave:
+s = cos(2*pi*freq*t);
+s = s/15;
 
-for k = 1:NIter
-    x = randn(FrameSize,1);
-    d = filt(x) + sinewave();
-    [y,e] = rls1(x,d);
-    w = rls1.Coefficients;
+noise = awgn(s, 60);
 
-    % scope(d,e)
-    %%
-    % Запись полного сигнала
-    xx((k-1)*length(x)+1:k*length(x)) = x;
-    dd((k-1)*length(d)+1:k*length(d)) = d;
-    yy((k-1)*length(y)+1:k*length(y)) = y;
-end
+s = s + noise;
+% create signal adc0_out Fs/2
+s0 = s(1:2:end); 
+% create signal adc1_out Fs/2
+s1 = s(2:2:end); 
+
+% add time skew
+delay = 1;
+s0 = [s0(delay:end); zeros(delay-1,1)];
+plot([s0(1:100), s1(1:100)]);
 
 
-function [n,x,s,fs] = genData(numPts, freq, filt, nVar, SNR)
-    % Generate time values
-    t = linspace(0,1,numPts)';
-    fs = numPts;
-    
-    % Generate tone
-    s = sin(2*pi*freq*t);
-    
-    % Generate noise
-    n = sqrt(nVar)*randn(numPts,1);
-    
-    % Filter noise
-    addnoise = filter(filt, 1, n);
-    
-    % Plot filter
-    freqz(filt,1,1000)
-    
-    % Adjust SNR of tone
-    s = s/sqrt(var(s)/(10^(SNR/10)*var(n)));
-    disp(['Calculated SNR = ' num2str(10*log10(var(s)/var(n)))])
-    
-    % Add noise to signal
-    x = s + addnoise;
-    
-end
+% emulate adc on odd and even
+x_adc0 = s0;% + noise;
+x_adc1 = s1;% + noise;
 
-numPts  = 1000;             % number of points to generate
-freq    = 100;              % frequency of fundamental tone
-filtord = 4;                % filter order
-filt    = rand(filtord, 1); % filter coefficients
-nVar    = 1;                % white noise variance
-SNR     = 10;              % signal to noise ratio of tone
-    
-N = 4;
 
-% Generate the data!
-[n,x,s,fs] = genData(numPts, freq, filt, nVar, SNR);
+% add offset
+x_adc1 = x_adc1 + 0.002;
+% plot([x_adc0,x_adc1]);
 
-y_out1 = [];
-x3 = zeros(N,N);
-x3(1,:) = x(1:N);
-x3(:,1) = x(1:N).';
+% add gain
+x_adc1 = x_adc1 * 1.4;
 
-w1 = inv(x3'*x3) * x3' * s(1:N);
-st = lsqr(x3, s(1:N));
+% create main signal from adc
+x_after_subadc = zeros(length(s),1);
+x_after_subadc(1:2:end) = x_adc0; 
+x_after_subadc(2:2:end) = x_adc1; 
 
-y_out = w1(1)*x(1) + w1(2)*x(2) + w1(3)*x(3) + w1(4)*x(4); % + st(5)*x(5) + st(6)*x(6) + st(7)*x(7) + st(8)*x(8) + st(9)*x(9) + st(10)*x(10);
-y_out1(1) = y_out;
+% x_after_subadc1 = zeros(length(s),1);
+% x_after_subadc1(1:2:end) = x_adc1; 
+% x_after_subadc1(2:2:end) = x_adc1; 
 
-% work
-% for i = 1:2
-%   if i == 1  
-%     x3 = [x(i), x(i+1); x(i+1), 0];
-%   else
-%     x3 = [x(i), x(i+1); x(i+1), x(i+2)];
-%   end
-%   y3 = [s(i); s(i+1)];
-%   w1 = inv(x3'*x3) * x3' * y3;
-%   st = lsqr(x3, y3);
-%   y = st(1)*x(i)+st(2)*x(i+1);
-% end
+spectrumScope = spectrumAnalyzer(SampleRate=Fs, ...            
+            AveragingMethod='exponential',ForgettingFactor=0.99, ...
+            YLimits=[-30 10],ShowLegend=true);
+spectrumScope([x_after_subadc]);
+
+
+for z = 1:1
+
+    y_out1 = [];
+    x3 = [];
+    y_out = 0;
+
+    % create matrix for input signal
+    for i = 1:N
+        x3(i,:) = x_adc1(i:N+i-1).';
+    end
+
+    w1 = (x3'*x3) \ x3' * x_adc0(1:N);
+    % dd = det(x3);
+    % ee = cond(x3);
+    % w1 = inv(x3'*x3) * x3' * s(1:N);
+    % st = lsqr(x3, s(1:N));
+
+    for k = 1:N
+        % y_out = w1(1)*x(j+1) + w1(2)*x(j+2) + w1(3)*x(j+3) + w1(4)*x(j+4);
+        y_out = y_out + w1(k)*x_adc1(k);
+    end
+    % y_out = w1(1)*x(1) + w1(2)*x(2) + w1(3)*x(3) + w1(4)*x(4); % + st(5)*x(5) + st(6)*x(6) + st(7)*x(7) + st(8)*x(8) + st(9)*x(9) + st(10)*x(10);
+    y_out1(1) = y_out;
+
+    % work
+    % for i = 1:2
+    %   x3 = [x(i), x(i+1); x(i+1), x(i+2)];
+    %   y3 = [s(i); s(i+1)];
+    %   w1 = inv(x3'*x3) * x3' * y3;
+    %   st = lsqr(x3, y3);
+    %   y = st(1)*x(i)+st(2)*x(i+1);
+    % end
 %%
-for j = 1:length(s)-N-10
 
-    x3(1,:) = [x3(1,(2:N)), x(j+N)];
-    x3(2:N,1) = x3(1,2:N); 
-
-    % x3(2,2) = x3(1,3);
-
-        % if j > 1
-        %     for i = 1:N-1
-        %         if i < N-1
-        %             x3(i+1,N-i) = x3(1,N);
-        %         elseif i >= N-1
-        %             for k = 1:N-1
-        %                 x3(k+1,N-k+1) = x(j+N+1);
-        %             end
-        %         end
-        %     end
-        % 
-        % end
-        
-        x4 = fliplr(x3);
-        
-        for k = 1:j
-            q = diag(x4,k)
-            ar = find(q == 0);
-            az = q(1);
-            q = zeros(N-1,1);
-            for i = 1:length(ar)
-                q(ar) = az;
-            end
-            x4 = diag(q,1);
-            x4 = fliplr(x4);
-            x3 = x4+x3;
+    for j = 1:length(x_adc0)-2*N
+    
+        % shift to left matrix input signal
+        for i = 1:N
+            x3(i,:) = [x3(i,2:N), 0];
+            x3(i,N) = x_adc1(j+N-1+i);
         end
 
+        % estimate coeff
+        % w1 = inv(x3'*x3) * x3' * x_adc0(j+1:N+j);
+        w1 = ((x3'*x3) \ x3') * x_adc0(j+1:N+j);
+        % st = lsqr(x3, x_adc0(j+1:N+j));
 
-        % x3(2,2) = x3(1,3);
-        % if j == 2
-        %     x3(2,3) = x3(1,4);
-        %     x3(3,2) = x3(1,4);
-        % elseif j == 3
-        %     x3(2,3) = x3(1,4);
-        %     x3(3,2) = x3(1,4);
-        % 
-        %     x3(2,4) = x(j+N+1);
-        %     x3(3,3) = x3(2,4);
-        %     x3(4,2) = x3(3,3);
-        % elseif j ==4
-        %     x3(2,3) = x3(1,4);
-        %     x3(3,2) = x3(1,4);
-        % 
-        %     x3(2,4) = x(j+N+1);
-        %     x3(3,3) = x3(2,4);
-        %     x3(4,2) = x3(3,3);
-        % 
-        %     x3(3,4) = x(j+N+2);
-        %     x3(4,3) = x3(3,4);
-        % elseif j > 5
-        %     x3(2,3) = x3(1,4);
-        %     x3(3,2) = x3(1,4);
-        % 
-        %     x3(2,4) = x(j+N+1);
-        %     x3(3,3) = x3(2,4);
-        %     x3(4,2) = x3(3,3);
-        % 
-        %     x3(3,4) = x(j+N+2);
-        %     x3(4,3) = x3(3,4);
-        % 
-        %     x3(4,4) = x(j+N+3);
-        % end
+        y_out = 0;
+        % filter input signal
+        for k = 1:N
+            % y_out = w1(1)*x(j+1) + w1(2)*x(j+2) + w1(3)*x(j+3) + w1(4)*x(j+4);
+            y_out = y_out + w1(k)*x_adc1(j+k);
+        end
 
+        y_out1(j+1) = y_out;
 
-    w1 = inv(x3'*x3) * x3' * s(j+1:N+j);
-    st = lsqr(x3, s(j+1:N+j));
+    end
 
-    % y_out = st(1)*x(j+1) + st(2)*x(j+2) + st(3)*x(j+3) + st(4)*x(j+4); % + st(5)*x(5) + st(6)*x(6) + st(7)*x(7) + st(8)*x(8) + st(9)*x(9) + st(10)*x(10);
-    y_out = w1(1)*x(j+1) + w1(2)*x(j+2) + w1(3)*x(j+3) + w1(4)*x(j+4);
-    y_out1(j+1) = y_out;
+    nn = 1855;
+
+    y_out11(1:nn,z) =  y_out1(1:nn).';
+    error_out(1:nn,z) = y_out1(1:nn).' ./ x_adc0(1:nn);
 
 end
 
-nn = 987;
+    subplot(2,1,1)
+    plot([x_adc0(1:nn), x_adc1(1:nn), y_out11(1:nn)])
+    % title('Отношение между отсчетами I-составляющей')
+    xlabel('Номер отсчета') 
+    ylabel('Амплитуда') 
+    legend({'Исходный сигнал','Сигнал + Шум','Сигнал с выхода адаптивного фильтра'},'Location','northeast')
+    subplot(2,1,2)
+    plot([error_out(:,1)]);% error_out(:,2), error_out(:,3)])
+    title('Относительная ошибка между исходным сигналом и выходом адаптивного фильтра')
+    xlabel('Номер отсчета') 
+    ylabel('Отношение') 
 
-y_out1 =  y_out1.';
-error_out = y_out1 ./ s(1:nn);
+    % create main signal from adc
+    x_after_adc = zeros(nn*2,1);
+    x_after_adc(1:2:end) = x_adc0(1:nn); 
+    x_after_adc(2:2:end) = y_out11; 
 
-subplot(2,1,1)
-plot([s(1:nn),x(1:nn), y_out1])
-% title('Отношение между отсчетами I-составляющей')
-xlabel('Номер отсчета') 
-ylabel('Амплитуда') 
-legend({'Исходный сигнал','Сигнал + Шум','Сигнал с выхода адаптивного фильтра'},'Location','northeast')
-subplot(2,1,2)
-plot(error_out)
-title('Относительная ошибка между исходным сигналом и выходом адаптивного фильтра')
-xlabel('Номер отсчета') 
-ylabel('Отношение') 
+    spectrumScope = spectrumAnalyzer(SampleRate=Fs, ...            
+                AveragingMethod='exponential',ForgettingFactor=0.99, ...
+                YLimits=[-30 10],ShowLegend=true);
+    spectrumScope([x_after_subadc(1:nn*2), x_after_adc]);
+
+
+
+
 %% N = 3
 % for j = 1:length(s)-N
 % 
@@ -262,105 +229,3 @@ ylabel('Отношение')
 % legend({'Исходный сигнал','Сигнал + Шум','Сигнал с выхода адаптивного фильтра'},'Location','northeast')
 
 %%
-
-% subplot(2,1,1)
-% plot([xx, dd, yy]);
-% subplot(2,1,2)
-% plot(e)
-
-
-
-% release(scope)
-
-    %--------------------------------------------------------------------------
-% Filtering
-%--------------------------------------------------------------------------
-% Filter Parameters
-p       = 1;                % filter order
-lambda  = 1.0;              % forgetting factor
-laminv  = 1/lambda;
-delta   = 1.0;              % initialization parameter
-% Filter Initialization
-w       = zeros(p,1);       % filter coefficients
-P       = delta*eye(p);     % inverse correlation matrix
-e       = x*0;              % error signal
-
-
-x2 = zeros(4,4);
-y1 = zeros(1,4);
-
-for m = p:length(x)
-    % Acquire chunk of data
-    y = n(m:-1:m-p+1);
-    % Error signal equation
-    e(m) = x(m)-w'*y;
-    r = x(m);
-    % Parameters for efficiency
-    Pi = P*y;
-    
-    % Filter gain vector update
-    k = (Pi)/(lambda+y'*Pi);
-    % Inverse correlation matrix update
-    P = (P - k*y'*P)*laminv;
-    % Filter coefficients adaption
-    w = w + k*e(m);
-    % Counter to show filter is working
-    %if mod(m,1000) == 0
-    %    disp([num2str(m/1000) ' of ' num2str(floor(length(x)/1000))])
-    %end
-
-    % x1 = x(m:-1:m-p+1);
-    x1 = x(1:4);
-    y1 = n(1:4);
-    x3 = zeros(2,2);
-    y3 = zeros(2,1);
-    x33 = zeros(2,2);
-    y33 = zeros(2,1);
-    % for i = 1:4
-    %     % x1 = [x(i), x1(1,1), x1(1,2), x1(1,3); x1(1,1), x1(2,1), x1(2,2), x1(2,3); ...
-    %     %     x1(2,1), x1(3,1), x1(3,2), x1(3,3);...
-    %     %     x1(3,1), x1(4,1), x1(4,2), x1(4,3)];
-    %     if i == 1
-    %         x2 = [x1(1); 0; 0; 0];
-    %     elseif i == 2
-    %         x2 = [x1(1); x1(2); 0; 0];
-    %     elseif i == 3
-    %         x2 = [x1(1); x1(2); x1(3); 0];
-    %     elseif i == 4
-    %         x2 = [x1(1); x1(2); x1(3); x1(4)];
-    %     end
-    % 
-    %     if i == 1
-    %         y1 = [n(1); 0; 0; 0];
-    %     elseif i == 2
-    %         y1 = [n(1); n(2); 0; 0];
-    %     elseif i == 3
-    %         y1 = [n(1); n(2); n(3); 0];
-    %     elseif i == 4
-    %         y1 = [n(1); n(2); n(3); n(4)];
-    %     end
-
-       % y1 = [n(i); y1(1); y1(2); y1(3)];
-     
-       % a = lsqr(x1(1), y1(1));
-       % b = lsqr(x1(2), y1(2));
-
-       % shift from first order
-       % for i = 1:2
-       %  x33 = [x(i), x33(1); x33(1,1), x33(2,1)];
-       %  y33 = [n(i), y33(1); y33(1,1), y33(2,1)];
-       %  w2 = inv(x33'*x33) * x33' * y33;
-       % end
-
-
-       % work
-       for i = 1:2
-        x3 = [x(i), x(i+1); x(i+1), x(i+2)];
-        % x3 = [x(i), x(2); x(2), 0];
-        % y3 = [n(1); n(2)];
-        y3 = [n(i); n(i+1)];
-        w1 = inv(x3'*x3) * x3' * y3;
-        st = lsqr(x3, y3);
-       end
- 
- end
